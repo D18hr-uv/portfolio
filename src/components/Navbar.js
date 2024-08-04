e009786597c7e8779df8101274b59b41e4d3dc30
Dhruv Bhardwaj <118291246+D18hr-uv@users.noreply.github.com>
2024-08-05 00:10:23 +0530

    Update Navbar.js for the github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://d18hr-uv.github.io/Portfolio/"
+                href="https://github.com/D18hr-uv/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
